11c8979ee8430aabdb4b435c5fd36fc0a13fbd2d
Karina Nowicka <knowicka@gmail.com>
2018-11-29 22:38:57 +0100

    form styling

--- js/components/Form.jsx
+++ js/components/Form.jsx
@@ -1,27 +1,37 @@
 import React from 'react';
 
 class Form extends React.Component {
+
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+
+        console.log('dziala');
+    }
+
+
+
     render() {
-        return <form className="form">
-            <div className="form-row">
-                <div className="form-item col-lg-6 col-md-8 mb-4">
-                    <label htmlFor="nickname">Your nickname</label>
-                    <input className="form-control" type="text" id="nickname"/>
+        return <section className="form-section">
+            <form className="form" onSubmit={e => this.handleSubmit(e)}>
+                <div className="form__item">
+                    <div className="label-box"><label className="label" htmlFor="nickname">Your nickname</label></div>
+                    <div className="input-box"><input className="input" type="text" id="nickname" placeholder="nickname"/></div>
                 </div>
-            </div>
-            <div className="form-row">
-                <div className="form-item col-lg-6 col-md-8 mb-4">
-                    <label htmlFor="nickname">Your email</label>
-                    <input className="form-control" type="text" id="email"/>
+                <div className="form__item">
+                    <div className="label-box"><label className="label" htmlFor="nickname">Your email</label></div>
+                    <div className="input-box"><input className="input" type="text" id="email" placeholder="email"/></div>
                 </div>
-            </div>
-            <div className="form-row">
-                <div className="form-item col-lg-6 col-md-8 mb-4">
-                    <label htmlFor="nickname">Your IP address</label>
-                    <input className="form-control" type="text" id="ip"/>
+                <div className="form__item">
+                    <div className="label-box"><label className="label" htmlFor="nickname">Your IP address</label></div>
+                    <div className="input-box"><input className="input" type="text" id="ip" placeholder="IP address"/></div>
                 </div>
-            </div>
-        </form>
+
+                <button className="submit-btn" type="submit">Add</button>
+
+            </form>
+        </section>
+
     }
 }
 
